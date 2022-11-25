fc5e71daa6bd50a152597085f61ad3e0c47e6c11
Gabriel Bernardo <bielcotrimsv@gmail.com>
2022-11-25 18:35:45 -0300

    req 18 sessão 1 completa

---
+++ app/backend/src/database/models/Matches.ts
@@ -0,0 +1,51 @@
+import { BOOLEAN, INTEGER, Model } from 'sequelize';
+import db from '.';
+import Teams from './Teams';
+
+export default class Matches extends Model {
+  declare id: number;
+  declare homeTeam: number;
+  declare homeTeamGoals: number;
+  declare awayTeam: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+Matches.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  underscored: true,
+  timestamps: false,
+});
+
+Matches.belongsTo(Teams, { foreignKey: 'homeTeam', as: 'teams' });
+Matches.belongsTo(Teams, { foreignKey: 'awayTeam', as: 'teams' });
+
+Teams.hasMany(Matches, { foreignKey: 'homeTeam', as: 'matches' });
+Teams.hasMany(Matches, { foreignKey: 'awayTeam', as: 'matches' });
